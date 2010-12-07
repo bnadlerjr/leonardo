8562c8394ec7abcfd2c7a9c6f47d7d00d325fd81
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-12-07 08:32:16 -0500

    Raphael.leonardo.getPaletteColor Spec
    * Add more specs.
    * Refactor so that reset option no longer gives the next color, it just resets
      the cycle and return "true".

--- spec/raphael.spec.js
+++ spec/raphael.spec.js
@@ -2,16 +2,43 @@
  * Raphael helper function specs.
  */
 describe("Raphael.leonardo.getPaletteColor", function () {
-    it("should return next color in sequence", function () {
-        var expectedColors = [
-                "rgb(229, 39, 54)",  "rgb(32, 141, 70)",  "rgb(43, 87, 169)",
-                "rgb(236, 125, 43)", "rgb(102, 38, 145)", "rgb(156, 25, 37)",
-                "rgb(175, 50, 148)"
-            ],
-            actualColors = [];
-
+    function compareColors(expectedColors) {
         expectedColors.forEach(function (value) {
             expect(value).toEqual(Raphael.leonardo.getPaletteColor());
         });
+    }
+
+    beforeEach(function() {
+        Raphael.leonardo.getPaletteColor({reset: true});
+    });
+
+    it("should return next color in sequence", function () {
+        compareColors([
+                "rgb(229, 39, 54)",  "rgb(32, 141, 70)",  "rgb(43, 87, 169)",
+                "rgb(236, 125, 43)", "rgb(102, 38, 145)", "rgb(156, 25, 37)",
+                "rgb(175, 50, 148)"
+        ]);
+    });
+
+    it("should restart cycle of colors when it reaches the last color", function () {
+        compareColors([
+                "rgb(229, 39, 54)",  "rgb(32, 141, 70)",  "rgb(43, 87, 169)",
+                "rgb(236, 125, 43)", "rgb(102, 38, 145)", "rgb(156, 25, 37)",
+                "rgb(175, 50, 148)", "rgb(229, 39, 54)",  "rgb(32, 141, 70)"
+        ]);
+    });
+
+    it("should not return a color if the reset option is given", function () {
+        expect(true).toEqual(Raphael.leonardo.getPaletteColor({reset: true}));
+    });
+
+    it("should reset the color cycle if the reset option is given", function () {
+        var expectedColors = [
+                "rgb(229, 39, 54)",  "rgb(32, 141, 70)",  "rgb(43, 87, 169)"
+            ];
+
+        compareColors(expectedColors);
+        Raphael.leonardo.getPaletteColor({reset: true});
+        compareColors(expectedColors);
     });
 });
