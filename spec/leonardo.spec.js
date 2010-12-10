669b97491db0b301a884b6de729a85f54ecc0bf4
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-12-10 16:35:15 -0500

    Specs for oneBar graph.
    * Fix extend function.
    * Specify options for oneBar and sparkline in functions instead of global
      options variable.
    * Temporarily create a defaultOptions variable to avoid default options from
      being overridden. Will be removed once all graphs define their default
      options inline.

--- spec/leonardo.spec.js
+++ spec/leonardo.spec.js
@@ -2,16 +2,16 @@
  * Leonardo Specs
  */
 describe("Leonardo", function () {
+    var graph;
+
     describe("sparkline", function () {
         var data = [3, 12, 7, 9];
 
         describe("with default options", function () {
-            var result;
-
             beforeEach(function () {
                 setFixtures(sandbox());
                 Leonardo.sparkline("sandbox", data);
-                result = document.getElementById("sandbox").firstChild;
+                graph = document.getElementById("sandbox").firstChild;
             });
 
             it("should draw the graph in the specified DOM ID", function () {
@@ -19,13 +19,13 @@ describe("Leonardo", function () {
             });
 
             it("should draw a graph with a path for each data point", function () {
-                var paths = result.getElementsByTagName('path');
+                var paths = graph.getElementsByTagName('path');
                 expect(paths.length).toEqual(3);
             });
 
             it("should draw the graph with the default size", function () {
-                expect(result).toHaveAttr("width", 300);
-                expect(result).toHaveAttr("height", 200);
+                expect(graph).toHaveAttr("width", 300);
+                expect(graph).toHaveAttr("height", 200);
             });
         });
 
@@ -33,10 +33,72 @@ describe("Leonardo", function () {
             it("should draw the graph with the specified size", function () {
                 setFixtures(sandbox());
                 Leonardo.sparkline("sandbox", data, {width:100, height:50});
-                var result = document.getElementById("sandbox").firstChild;
+                graph = document.getElementById("sandbox").firstChild;
 
-                expect(result).toHaveAttr("width", 100);
-                expect(result).toHaveAttr("height", 50);
+                expect(graph).toHaveAttr("width", 100);
+                expect(graph).toHaveAttr("height", 50);
+            });
+        });
+    });
+
+    describe("oneBar", function () {
+        var bar;
+
+        describe("with default options", function () {
+            beforeEach(function () {
+                setFixtures(sandbox());
+                Leonardo.oneBar("sandbox", 0.3);
+                graph = document.getElementById("sandbox").firstChild;
+                bar = graph.getElementsByTagName('rect')[0];
+            });
+
+            it("should draw the graph in the specified DOM ID", function () {
+                expect(document.getElementById("sandbox")).toContain("svg");
+            });
+
+            it("should draw a rectangle that represents the data point", function () {
+                expect(bar).toHaveAttr("x", 0);
+                expect(bar).toHaveAttr("y", 1);
+                expect(bar).toHaveAttr("width", 90);
+                expect(bar).toHaveAttr("height", 198);
+            });
+
+            it("should draw the graph with the default size", function () {
+                expect(graph).toHaveAttr("width", 300);
+                expect(graph).toHaveAttr("height", 200);
+            });
+
+            it("should draw the graph with the first palette color", function () {
+                expect(bar).toHaveAttr("fill", "#e52736");
+            });
+        });
+
+        describe("with user defined options", function () {
+            beforeEach(function () {
+                setFixtures(sandbox());
+                Leonardo.oneBar("sandbox", 0.25, {
+                    width:          100,
+                    height:         30,
+                    fillColor:      "#333333",
+                    showPercentage: true
+                });
+                graph = document.getElementById("sandbox").firstChild;
+                bar = graph.getElementsByTagName('rect')[0];
+            });
+
+            it("should draw the graph with the specified size", function () {
+                expect(graph).toHaveAttr("width", 100);
+                expect(graph).toHaveAttr("height", 30);
+            });
+
+            it("should draw the graph with the specified palette color", function () {
+                expect(bar).toHaveAttr("fill", "#333333");
+            });
+
+            it("should show the percentage", function () {
+                var text = graph.getElementsByTagName("text")[0];
+                expect(text).toHaveAttr("text-anchor", "start");
+                expect(text.firstChild).toHaveText("25%");
             });
         });
     });
